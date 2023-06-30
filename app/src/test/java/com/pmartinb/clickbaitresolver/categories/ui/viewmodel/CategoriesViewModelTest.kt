62f2f8ce0660c9b6743e1552053dbbbec86d116e
Pablo Martín <pablo.martinblanco@telefonica.com>
2023-06-30 15:13:38 +0200

    Add unit test and Github action

---
+++ app/src/test/java/com/pmartinb/clickbaitresolver/categories/ui/viewmodel/CategoriesViewModelTest.kt
@@ -0,0 +1,30 @@
+package com.pmartinb.clickbaitresolver.categories.ui.viewmodel
+
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.flow.firstOrNull
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class CategoriesViewModelTest {
+
+    private lateinit var sut: CategoriesViewModel
+
+    @Before
+    fun init() {
+        sut = CategoriesViewModel()
+    }
+
+    @Test
+    fun `check correct initial categories`() = runTest {
+        assertEquals(
+            sut.categories.firstOrNull(),
+            INITIAL_CATEGORIES
+        )
+    }
+    private companion object {
+        val INITIAL_CATEGORIES = listOf<String>("Apple", "Space")
+    }
+}
