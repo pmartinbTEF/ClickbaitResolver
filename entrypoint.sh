6ba11d3ae4ce0da391adf249358f43f444a65664
Pablo Martín <pablo.martinblanco@telefonica.com>
2023-08-11 14:48:15 +0200

    Quick fix 25

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ echo "::debug ::Debug message"
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
+echo $time
 echo "::group::Some expandable logs"
 echo "Some logs"
 echo "Some logs"
