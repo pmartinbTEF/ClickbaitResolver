63b366a01939a433a96f4eec6d3e49b456dea34f
Pablo Martín <pablo.martinblanco@telefonica.com>
2023-08-25 10:50:24 +0200

    Quick fix4

---
+++ action3/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+echo "::debug ::Debug message"
+
+echo "Hello3 $1"
+otheroutput=$(date)
+echo "time=otheroutput" >> $GITHUB_OUTPUT
+echo $otheroutput
+echo "::group::Some expandable logs"
+echo "Some logs"
+echo "Some logs"
+echo "Some logs"
+echo "::endgroup::"
